ec6d8c16234e45a15baf93bb4f118cf64d4dbc21
GABRIEL PEDRO RAMALHO <biel-pedro@hotmail.com>
2023-10-16 10:41:35 -0300

    add banner

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import styled from "styled-components"
 import EstilosGlobais from "./componentes/EstilosGlobais"
 import Cabecalho from "./componentes/Cabecalho"
 import BarraLateral from "./componentes/BarraLateral"
+import Banner from "./componentes/Banner"
 
 const FundoGradiente = styled.div`
   background: linear-gradient(174.61deg, #041833 4.16%, #04244F 48%, #154580 96.76%);
@@ -16,6 +17,10 @@ function App() {
       <EstilosGlobais/>
       <Cabecalho />
       <BarraLateral />
+      <Banner 
+        backgroundImage='/imagens/banner.png' 
+        texto='App Space'
+      />
     </FundoGradiente>
   )
 }
